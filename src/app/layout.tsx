0c9faf00826c33a992bae15c1e9252907ba22738
Copalla-ttr <l3bdv.n1kt@yandex.ru>
2023-12-07 19:39:05 +0300

    h

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/header'
-import { createContext } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" suppressHydrationWarning={true}>
+    <html lang="en">
       <body className={inter.className}>
           <Header/>
           {children}
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Data, userContext } from "@/lib/context"
 import Button from "@mui/material/Button"
 import Dialog from "@mui/material/Dialog"
@@ -6,10 +8,8 @@ import Grid from "@mui/material/Grid"
 import Link from "@mui/material/Link"
 import List from "@mui/material/List"
 import ListItem from "@mui/material/ListItem"
-import ListItemAvatar from "@mui/material/ListItemAvatar"
 import ListItemButton from "@mui/material/ListItemButton"
 import ListItemText from "@mui/material/ListItemText"
-import Typography from "@mui/material/Typography"
 import { useContext, useState } from "react"
 
 export interface SimpleDialogProps {
